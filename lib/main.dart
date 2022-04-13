5af34a1a23c054bde70bf18022659cc8154a57b2
AleLevi <ale.levi@hotmail.com>
2022-04-13 09:15:56 -0300

    commit_first_funciton_add_to_do_list

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+// @dart=2.9
+// ignore_for_file: prefer_const_constructors, import_of_legacy_library_into_null_safe, unnecessary_import, unused_import, deprecated_member_use, unused_element, missing_required_param
+
+import 'dart:io';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newTodo = Map();
+      newTodo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newTodo["ok"] = false;
+      _toDoList.add(newTodo);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Terefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  controller: _toDoController,
+                  decoration: InputDecoration(
+                    labelText: "Nova Tarefa",
+                    labelStyle: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+              ),
+              RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  onPressed: _addToDo),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: 10.0),
+            itemCount: _toDoList.length,
+            itemBuilder: (context, index) {
+              return CheckboxListTile(
+                title: Text(_toDoList[index]["title"]),
+                value: _toDoList[index]["ok"],
+                secondary: CircleAvatar(
+                  child:
+                      Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+                ),
+                onChanged: (c) {
+                  setState(() {
+                    _toDoList[index]["ok"] = c;
+                  });
+                  ;
+                },
+              );
+            },
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {}
+    return null;
+  }
+}
